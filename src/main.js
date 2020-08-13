e9474c761a85c75276fa3b0c912031a997f9ea03
Mars <zm03322161@qq.com>
2020-08-13 17:01:56 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/zh-CN"; // lang i18n
+import VCharts from "v-charts";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI, { locale });
+Vue.use(VCharts);
 
 new Vue({
   router,
